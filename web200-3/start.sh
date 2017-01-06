1c01473d28d909f593ab9bb3ae1a425ae7de8254
wonderkun <wangxiaoman@mail.nwpu.edu.cn>
2017-01-06 18:50:03 +0800

    web20-3

---
+++ web200-3/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+service nginx restart
+service php7.0-fpm start
+service mysql restart  
+
+# iptables 默认在docker中不允许运行的  需要加 --privileged=true 参数 
+iptables   -t filter -A OUTPUT -p tcp --tcp-flags ALL SYN  -j DROP
+iptables  -t filter  -A OUTPUT -p upd  -j DROP 
+
+
+
+/usr/bin/tail -f /dev/null
