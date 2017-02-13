696c996cfeb7c04f4dce4f5b7fed30dd1c2dde15
wonderkun <wangxiaoman@mail.nwpu.edu.cn>
2017-02-13 14:34:23 +0800

    ok

--- web500-2/config.inc.php
+++ web500-2/config.inc.php
@@ -1,16 +1,13 @@
 <?php
 
 error_reporting(0);
-
 session_start();
-
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $database="taolu";
 
 // 创建连接
-
 $conn = mysqli_connect($servername, $username, $password,$database) or die(" connect to mysql error");
 $conn->query("set names 'utf8'");
 
--- web500-2/db.sql
+++ web500-2/db.sql
@@ -26,28 +26,19 @@ create table `page` (
     `num` varchar(255)  not null
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 drop table if exists `flags`;
 
 create table `flags` (
-
       `id` tinyint not null primary key ,
       `flag` varchar(50) not null 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into `flags` (id,flag) values (1,'CTF{945ad55928c3e9773c7y3cf2cf02cdbd}');
-
 insert into  `user`(id,uname,password,level)values(
-
      1,'admin','21232f297a57a5a743894aoe4a801fc3',1
 ),(2,'test','202cb962ac59075b964b07152d234b70',0);
-
 
 insert into  `page` values (20);
 insert into   `note` (title,content,userid)values(
      '测试笔记','这是管理员发布的测试笔记,个人无法删除(hint:./dbinit.sql)',1
 )
-
-
-
-
